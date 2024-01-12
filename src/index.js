ccd9c9c9dfcfedb747a17babe2ee9240d42975a0
octaviagf <oagf0616@gmail.com>
2024-01-11 21:57:50 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const services = [
+  {
+    name: "Classic haircut",
+    img: "img/classic.PNG",
+  },
+  { name: "Beard", img: "img/beard.PNG" },
+  { name: "Modern haircut", img: "img/modern.jpg" },
+  { name: "Classic/Modern haircut + Beard", img: "img/combo.PNG" },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="text">The Giants BarberShop</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="main">
+      <h2>Our Services</h2>
+      <ul>
+        {services.map((item) => (
+          <Service serviceObj={item} key={item.name} />
+        ))}
+      </ul>
+    </main>
+  );
+}
+function Service(props) {
+  return (
+    <li className="item">
+      <div>
+        <img src={props.serviceObj.img} alt={props.serviceObj.name} />
+        <h3>{props.serviceObj.name}</h3>
+      </div>
+    </li>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
